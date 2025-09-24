be884c5a792573939dd2d7a35b1de191758e4e83
Jacob Toone <jacobrtoone@gmail.com>
2025-09-24 10:44:19 -0600

    basic login form functionality

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ function foo() {
     alert("Foo was called!");
     document.getElementById("output").innerText = "Foo was called!";
 }
-
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginForm").addEventListener("submit", function (event) {
+      event.preventDefault(); // stop page from reloading
+      login(); // call your login function
+    });
+  });
+  
 function login() {
     const user = document.getElementById("username").value;
     const pass = document.getElementById("password").value;
