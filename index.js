ab878ec7b3394c1b626f6d50c631b4bd38456764
Jacob Toone <jacobrtoone@gmail.com>
2025-09-24 10:37:31 -0600

    added basic username and password box with coresponding JS

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function foo() {
     alert("Foo was called!");
+    document.getElementById("output").innerText = "Foo was called!";
+}
+
+function login() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    alert("Username: " + user + "\nPassword: " + pass);
+    document.getElementById("output").innerText =
+    "Username: " + user + " | Password: " + pass;
 }
